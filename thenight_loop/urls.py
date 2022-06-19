9b22b87f30766096f89c4eba374f879786b83644
FloKots <florencekotohoyoh@gmail.com>
2022-06-19 14:19:34 +0300

    add authentication templates

--- thenight_loop/urls.py
+++ thenight_loop/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
     path('profile/', user_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    # path('api/profile/', user_views.ProfileList.as_view(), name='profile-list'),
+    path('api/profile/', user_views.ProfileList.as_view(), name='profile-list'),
     # path('', include('main.urls')),
 ]
 

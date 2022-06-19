276ec3eedad9bf021c70b5d187972904cce26b13
FloKots <florencekotohoyoh@gmail.com>
2022-06-19 14:10:39 +0300

    add class Profile test cases

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,37 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Profile
 
 # Create your tests here.
+
+class ProfileTestClass(TestCase):
+        # Set up method
+    def setUp(self):
+        self.new_user = User(id=1, username='new_user')
+        self.new_profile = Profile(id=1, bio='Test Bio', image='../media/profile_pics/ruby.jpg', user=self.new_user)
+
+    def tearDown(self):
+        User.objects.all().delete()
+        Profile.objects.all().delete()
+
+    # Testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_profile, Profile))
+
+    # Testing save method
+    def test_save(self):
+        self.new_profile.save()
+        profiles = Profile.objects.all()
+        self.assertTrue(len(profiles) > 0)
+
+    
+    def test_delete(self):
+        self.new_profile.delete()
+        profiles = Profile.objects.all()
+        self.assertTrue(len(profiles) == 0)
+    
+
+    def test_update(self):
+        self.new_profile.bio = 'New Bio'
+        self.new_profile.save()
+        self.assertEqual(self.new_profile.bio, 'New Bio')

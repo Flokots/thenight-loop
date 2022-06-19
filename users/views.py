adcca2def676c3ecc9eba42014431e1351fe9409
FloKots <florencekotohoyoh@gmail.com>
2022-06-19 14:11:32 +0300

    add users app view methods

--- users/views.py
+++ users/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+
+from users.models import Profile
+from .serializer import ProfileSerializer
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Your account has been created! You are now able to log in!')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+
+    title='Register'
+    return render(request, 'users/register.html', {'form': form, 'title': title})
+
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+
+            messages.success(request, f'Your account has been updated successfully!')
+
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    title='Profile'
+    return render(request, 'users/profile.html', {'u_form': u_form, 'p_form': p_form, 'title': title})
+
+
+
+class ProfileList(APIView):
+    def get(self, request, format=None):
+        all_profiles = Profile.objects.all()
+        serializers = ProfileSerializer(all_profiles, many=True)
+        return Response(serializers.data)

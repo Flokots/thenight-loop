b27aed2ce3bd017ba887cccf0d0c71adb43487ab
FloKots <florencekotohoyoh@gmail.com>
2022-06-20 11:31:40 +0300

    add post list view

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from .views import PostListView
 from . import views
 
 urlpatterns=[
-    path('', views.index, name='index')
+    path('', PostListView.as_view(), name='index'),
+
 ]
--- main/views.py
+++ main/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
-from .models import Post
+from .models import Post, Business, Contact, Neighborhood
+from django.views.generic import ListView
 
 # Create your views here.
 def index(request):
     title='Home'
-    posts =Post.objects.all()
-    return render(request, 'index.html', {'title': title, 'posts':posts})
+    return render(request, 'index.html', {'title': title})
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'index.html' # Expected </app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
 

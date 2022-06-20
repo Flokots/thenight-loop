a3242e66ce9600884aba233c5d720f48412e0fa8
FloKots <florencekotohoyoh@gmail.com>
2022-06-21 02:01:27 +0300

    register contact model

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Neighborhood, Business, Post
+from .models import Neighborhood, Business, Post, Contact
 
 # Register your models here.
 admin.site.register(Neighborhood)
 admin.site.register(Business)
 admin.site.register(Post)
+admin.site.register(Contact)

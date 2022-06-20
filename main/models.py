f755cd468860785d964310e173a53b007fc62de1
FloKots <florencekotohoyoh@gmail.com>
2022-06-20 11:02:49 +0300

    add contact model

--- main/models.py
+++ main/models.py
@@ -116,3 +116,39 @@ class Post(models.Model):
         self.last_updated = timezone.localtime(timezone.now())
         super(Post, self).save(*args, **kwargs)
 
+
+
+class Contact(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    phone_number = models.CharField(null=True, blank=True, max_length=200)
+    email_address = models.EmailField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    # Utility Variables
+    uniqueId = models.CharField(null=True, blank=True, max_length=100)
+    slug = models.SlugField(max_length=500, unique=True, blank=True, null=True)
+    date_created = models.DateTimeField(blank=True, null=True)
+    last_updated = models.DateTimeField(blank=True, null=True)
+ 
+   
+    def __str__(self):
+        return '{} {}'.format(self.title, self.uniqueId)
+
+    
+    def get_absolute_url(self):
+        return reverse('contact-detail', kwargs={'slug': self.slug})
+
+    def save(self, *args, **kwargs):
+        if self.date_created is None:
+            self.date_created = timezone.localtime(timezone.now())
+        
+        if self.uniqueId is None:
+            self.uniqueId = str(uuid4()).split('-')[4]
+            self.slug = slugify('{} {}'.format(self.title, self.uniqueId))
+
+        self.slug = slugify('{} {}'.format(self.title, self.uniqueId))
+        self.last_updated = timezone.localtime(timezone.now())
+        super(Post, self).save(*args, **kwargs)
+

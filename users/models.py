1890ca88f5e8c54b04a02060c7a006609d4d8e0d
FloKots <florencekotohoyoh@gmail.com>
2022-06-20 07:49:49 +0300

    add neighborhood, location fields to profile

--- users/models.py
+++ users/models.py
@@ -2,10 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
 
+from main.models import Neighborhood
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.CharField(max_length=200, null=True)
     image = models.ImageField(default='default_user.jpg', upload_to='profile_pics/')
+    neighborhood = models.ForeignKey(Neighborhood, on_delete=models.CASCADE, blank=True, null=True)
+    location = models.CharField(null=True, blank=True, max_length=200)
 
     def __str__(self):
         return f'{self.user.username} Profile'

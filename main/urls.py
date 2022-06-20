4af49d7f2b39eb581dacd5fe5b13de0d8c021fa2
FloKots <florencekotohoyoh@gmail.com>
2022-06-20 12:26:08 +0300

    add post-detail template

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import PostListView
+from .views import PostListView, PostDetailView
 from . import views
 
 urlpatterns=[
     path('', PostListView.as_view(), name='index'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
 
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Post, Business, Contact, Neighborhood
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
 def index(request):
@@ -12,4 +12,23 @@ class PostListView(ListView):
     model = Post
     template_name = 'index.html' # Expected </app>/<model>_<viewtype>.html
     context_object_name = 'posts'
+    ordering = ['-date_posted']
 
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html' 
+
+
+class BusinessListView(ListView):
+    model = Business
+    template_name = 'business.html' # Expected </app>/<model>_<viewtype>.html
+    context_object_name = 'businesses'
+    ordering = ['-date_posted']
+
+
+class ContactListView(ListView):
+    model = Contact
+    template_name = 'contact.html' # Expected </app>/<model>_<viewtype>.html
+    context_object_name = 'contacts'
+    ordering = ['-date_posted']

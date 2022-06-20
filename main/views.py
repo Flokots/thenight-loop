b3d11c251f2c05dfcb3981d4568d5467b3eeddb2
FloKots <florencekotohoyoh@gmail.com>
2022-06-20 10:25:34 +0300

    style post card

--- main/views.py
+++ main/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 def index(request):
     title='Home'
-    return render(request, 'index.html', {'title': title})
+    posts =Post.objects.all()
+    return render(request, 'index.html', {'title': title, 'posts':posts})
 

d121bd1585827915c42201f591edb8e948b3fa4e
FloKots <florencekotohoyoh@gmail.com>
2022-06-20 07:55:40 +0300

    register business model

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Neighborhood
+from .models import Neighborhood, Business
 
 # Register your models here.
 admin.site.register(Neighborhood)
+admin.site.register(Business)

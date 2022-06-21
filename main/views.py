a18bdf15b2212ba4ae9e476920ede79cf4ebad8f
FloKots <florencekotohoyoh@gmail.com>
2022-06-21 03:32:19 +0300

    add create view methods for models

--- main/views.py
+++ main/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from .models import Post, Business, Contact, Neighborhood
-from django.views.generic import ListView, DetailView
+from django.views.generic import (
+    ListView, 
+    DetailView,
+    CreateView,
+)
 
 # Create your views here.
 def index(request):
@@ -15,9 +19,20 @@ class PostListView(ListView):
     ordering = ['-date_posted']
 
 
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'description', 'post_image']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        form.instance.neighborhood = self.request.user.profile.neighborhood
+        return super().form_valid(form)
+
+
 class PostDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html' 
+
 
 
 class BusinessListView(ListView):
@@ -27,8 +42,41 @@ class BusinessListView(ListView):
     ordering = ['-date_created']
 
 
+class BusinessCreateView(CreateView):
+    model = Business
+    fields = ['name', 'email_address', 'business_image']
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        form.instance.neighborhood = self.request.user.profile.neighborhood
+        return super().form_valid(form)
+
+
+
+
 class ContactListView(ListView):
     model = Contact
     template_name = 'contact.html' # Expected </app>/<model>_<viewtype>.html
     context_object_name = 'contacts'
     ordering = ['-date_posted']
+
+
+class ContactCreateView(CreateView):
+    model = Contact
+    fields = ['title', 'description', 'phone_number', 'email_address']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        form.instance.neighborhood = self.request.user.profile.neighborhood
+        return super().form_valid(form)
+
+
+
+class NeighborhoodCreateView(CreateView):
+    model = Post
+    fields = ['name', 'location', 'occupants_count', 'hood_image']
+
+    def form_valid(self, form):
+        form.instance.admin = self.request.user
+        return super().form_valid(form)
+

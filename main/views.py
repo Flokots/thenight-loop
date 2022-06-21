a9ac1382dc6b02d129af3629d40b80ba930b19f9
FloKots <florencekotohoyoh@gmail.com>
2022-06-21 03:42:53 +0300

    add update, delete view methods

--- main/views.py
+++ main/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from .models import Post, Business, Contact, Neighborhood
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import (
     ListView, 
     DetailView,
     CreateView,
+    UpdateView,
+    DeleteView,
 )
 
 # Create your views here.
@@ -34,6 +37,34 @@ class PostDetailView(DetailView):
     template_name = 'post_detail.html' 
 
 
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'description', 'post_image']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        form.instance.neighborhood = self.request.user.profile.neighborhood
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+
 
 class BusinessListView(ListView):
     model = Business
@@ -51,6 +82,32 @@ class BusinessCreateView(CreateView):
         form.instance.neighborhood = self.request.user.profile.neighborhood
         return super().form_valid(form)
 
+
+class BusinessUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Business
+    fields = ['name', 'email_address', 'business_image']
+
+    def form_valid(self, form):   
+        form.instance.owner = self.request.user
+        form.instance.neighborhood = self.request.user.profile.neighborhood
+        return super().form_valid(form)
+
+    def test_func(self):
+        business = self.get_object()
+        if self.request.user == business.owner:
+            return True
+        return False
+
+
+class BusinessDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Business
+    success_url = '/'
+
+    def test_func(self):
+        business = self.get_object()
+        if self.request.user == business.owner:
+            return True
+        return False
 
 
 
@@ -71,12 +128,63 @@ class ContactCreateView(CreateView):
         return super().form_valid(form)
 
 
+class ContactUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Contact
+    fields = ['title', 'description', 'phone_number', 'email_address']
+
+    def form_valid(self, form):   
+        form.instance.author = self.request.user
+        form.instance.neighborhood = self.request.user.profile.neighborhood
+        return super().form_valid(form)
+
+    def test_func(self):
+        contact = self.get_object()
+        if self.request.user == contact.author:
+            return True
+        return False
+
+
+class ContactDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Contact
+    success_url = '/'
+
+    def test_func(self):
+        contact = self.get_object()
+        if self.request.user == contact.author:
+            return True
+        return False
+
+
 
 class NeighborhoodCreateView(CreateView):
-    model = Post
+    model = Neighborhood
     fields = ['name', 'location', 'occupants_count', 'hood_image']
 
     def form_valid(self, form):
         form.instance.admin = self.request.user
         return super().form_valid(form)
 
+class NeighborhoodUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Neighborhood
+    fields = ['name', 'location', 'occupants_count', 'hood_image']
+
+    def form_valid(self, form):   
+        form.instance.admin = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        neighborhood = self.get_object()
+        if self.request.user == neighborhood.admin:
+            return True
+        return False
+
+
+class NeighborhoodDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Neighborhood
+    success_url = '/'
+
+    def test_func(self):
+        neighborhood = self.get_object()
+        if self.request.user == neighborhood.admin:
+            return True
+        return False
